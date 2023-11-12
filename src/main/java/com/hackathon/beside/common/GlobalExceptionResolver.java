b75a0537d5dcab8d5d4b1b99f41974219e3c9750
J-MU <alsdnrdl001@gmail.com>
2023-11-12 14:22:23 +0900

    [feat] exception http status code 수정

--- src/main/java/com/hackathon/beside/common/GlobalExceptionResolver.java
+++ src/main/java/com/hackathon/beside/common/GlobalExceptionResolver.java
@@ -5,27 +5,33 @@ import com.hackathon.beside.common.exception.DuplicateNickname;
 import com.hackathon.beside.common.response.FailResponse;
 import com.hackathon.beside.common.exception.ResourceNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionResolver {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ResourceNotFoundException.class)
     public FailResponse resolveNotFoundException(Exception e) {
         return new FailResponse(e.getMessage());
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({ConstraintViolationException.class})
     public FailResponse resolveException(Exception e) {
         return new FailResponse(e.getMessage());
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({DuplicateNickname.class})
     public FailResponse resolveDuplicateNickname(Exception e) {
         return new FailResponse("중복된 닉네임 입니다.");
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({DuplicateAccount.class})
     public FailResponse resolveDuplicateAccount(Exception e) {
         return new FailResponse("중복된 아이디 입니다.");
