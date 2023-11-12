1fde5d1bb75a1ead4e4a7486f91e1bc192e74c53
J-MU <alsdnrdl001@gmail.com>
2023-11-12 19:44:33 +0900

    [refactor] fcmToken 추가

--- src/main/java/com/hackathon/beside/common/entity/User.java
+++ src/main/java/com/hackathon/beside/common/entity/User.java
@@ -27,6 +27,7 @@ public class User {
     private Long readNewsCount;
     private Long enteredQuizCount;
     private Float correctRate;
+    private String fcmToken;
     private UserAuthority authority;
 
     @ManyToOne(fetch = FetchType.LAZY)
